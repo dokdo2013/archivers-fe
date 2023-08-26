bdcc46fb963fef076da0d3ffeb3e7581bc32b277
hyeonwoojo <h_jo@ejn.gg>
2023-08-26 12:43:53 +0900

    fix: change next config to standalone mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
   images: {
     domains: ["videoimg.afreecatv.com", "iflv14.afreecatv.com"],
   },
