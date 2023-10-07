1ba533c4e3f8bed471fd8209d4d9a660bb8b3c03
hyeonwoojo <h_jo@ejn.gg>
2023-10-07 13:27:43 +0900

    feat: Add codenbutter

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,11 @@ export default function Document() {
       <Head>
         <meta name="description" content="아카이버스 - Archivers" />
         <link rel="icon" href="/Archivers_A.png" />
+        <script
+          src="https://buttr.dev/butter.js"
+          data-site-id="iwmpxkcmmf"
+          async
+        ></script>
       </Head>
       <body>
         <Main />
