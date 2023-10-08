ef80c95ec29769b1e070c5d4810e80e491732e87
hyeonwoojo <h_jo@ejn.gg>
2023-10-08 15:11:24 +0900

    feat: add ga

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -6,6 +7,26 @@ export default function Document() {
       <Head>
         <meta name="description" content="아카이버스 - Archivers" />
         <link rel="icon" href="/Archivers_A.png" />
+
+        {/* GTag (GA4) */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-FVFER73C3X`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FVFER73C3X');
+            `,
+          }}
+        />
+
+        {/* CodeNButter */}
         <script
           src="https://buttr.dev/butter.js"
           data-site-id="iwmpxkcmmf"
